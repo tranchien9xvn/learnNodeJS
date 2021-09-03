e247a23d8ccf84880e77cf2ef1d4d95b0423e08b
tranchien9xvn <tranchien9xvn@gmail.com>
2021-09-03 20:37:37 +0700

    them thu vien src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 const port = 3000
+
+app.use(morgan('combined'))
 
 app.get('/tin-tuc', (req, res) => {
   res.send('Hello World!')
